d0769da3468f02f4837f53d223eb936b16ab8560
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-07-23 18:20:58 +0200

    remove test feature

--- src/main/java/com/tristankechlo/explorations/init/ModFeatures.java
+++ src/main/java/com/tristankechlo/explorations/init/ModFeatures.java
@@ -2,9 +2,7 @@ package com.tristankechlo.explorations.init;
 
 import com.tristankechlo.explorations.Explorations;
 import com.tristankechlo.explorations.worlgen.features.ScarecrowFeature;
-import com.tristankechlo.explorations.worlgen.features.SphereFeature;
 import com.tristankechlo.explorations.worlgen.features.config.ScarecrowFeatureConfig;
-import com.tristankechlo.explorations.worlgen.features.config.SphereFeatureConfig;
 
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -15,7 +13,6 @@ public class ModFeatures {
 
 	public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(Registry.FEATURE_REGISTRY, Explorations.MOD_ID);
 
-	public static final RegistryObject<Feature<SphereFeatureConfig>> SPHERE = FEATURES.register("sphere", () -> new SphereFeature(SphereFeatureConfig.CODEC));
 	public static final RegistryObject<Feature<ScarecrowFeatureConfig>> SCARECROW = FEATURES.register("scarecrow", () -> new ScarecrowFeature(ScarecrowFeatureConfig.CODEC));
 
 }
