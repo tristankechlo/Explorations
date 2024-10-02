aaeec5600fa1e2c6630530f6d97d8f649ea30087
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2024-10-02 20:47:12 +0200

    Update StructureTemplatePoolAccessor.java

--- common/src/main/java/com/tristankechlo/explorations/mixin/StructureTemplatePoolAccessor.java
+++ common/src/main/java/com/tristankechlo/explorations/mixin/StructureTemplatePoolAccessor.java
@@ -4,14 +4,12 @@ import com.mojang.datafixers.util.Pair;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.List;
 
-@Debug(export = true)
 @Mixin(StructureTemplatePool.class)
 public interface StructureTemplatePoolAccessor {
 
